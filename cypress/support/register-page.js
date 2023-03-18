a446a49afa6a88bd4d7a36e896229ec0ef5c5d2b
anisanursekararum <anisanursekararum@gmail.com>
2023-03-18 21:34:55 +0700

    register case negative

--- cypress/support/register-page.js
+++ cypress/support/register-page.js
@@ -9,6 +9,12 @@ class registerPage {
     conf_password = '#ConfirmPassword'
     register_button = '#submit'
     register_success = '.label-success'
+    danger_firstname = '#FirstName-error'
+    danger_surname = '#Surname-error'
+    danger_username = '#Username-error'
+    danger_password = '#Password-error'
+    danger_mismatch = '#ConfirmPassword-error'
+    danger_username_exist = '.label-danger'
 }
 
 export default registerPage;
